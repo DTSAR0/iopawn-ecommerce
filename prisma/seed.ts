78050a4b4823bf3025333cdcd1f84de88da77b02
dmytro <dimatsaryuk30@gmail.com>
2025-07-19 21:29:08 +0200

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,16 +15,16 @@ async function main() {
   });
 
   const existingProduct = await db.product.findUnique({
-    where: { slug: "silver-ring" }
+    where: { slug: "silver-ring-1" }
   });
 
   if (!existingProduct) {
     await db.product.create({
       data: {
-        name: "Silver Ring",
-        slug: "silver-ring",
-        description: "925 Sterling Silver Ring",
-        priceCents: 3999,
+        name: "Silver Ring 1",
+        slug: "silver-ring-1",
+        description: "925 Sterling Silver Ring 1",
+        priceCents: 3000,
         categoryId: category.id
       }
     });
